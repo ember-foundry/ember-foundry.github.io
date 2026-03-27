89d5bb90bff18a6670dedd6c3c751310e0377424
Craig Wayne <craigiswayne@gmail.com>
2026-03-27 13:30:10 +0000

    Player Select for desktop is done

--- projects/component-library/src/components/player-select/player-select.component.ts
+++ projects/component-library/src/components/player-select/player-select.component.ts
@@ -1,28 +1,23 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
-import {form} from '@angular/forms/signals';
 
 const states = ['human', 'cpu', 'inactive'] as const;
 type PlayerState = typeof states[number];
 
-const AVAILABLE_CHARACTERS: {state: PlayerState, name: string, image: string}[] = [
+const AVAILABLE_CHARACTERS: { state: PlayerState, image: string }[] = [
   {
     state: 'human',
-    name: 'Rory',
     image: 'http://localhost:4200/assets/images/pose-rory-transparent.png'
   },
   {
     state: 'human',
-    name: 'Ahmed',
     image: 'http://localhost:4200/assets/images/pose-ahmed-transparent.png'
   },
   {
     state: 'human',
-    name: 'Kim',
     image: 'http://localhost:4200/assets/images/pose-kim-transparent.png'
   },
   {
     state: 'human',
-    name: 'Tumi',
     image: 'http://localhost:4200/assets/images/pose-tumi-transparent.png'
   }
 ];
@@ -34,19 +29,28 @@ const AVAILABLE_CHARACTERS: {state: PlayerState, name: string, image: string}[] 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayerSelectComponent {
-    protected players = signal(AVAILABLE_CHARACTERS);
-    private readonly player_selection_form = form(this.players);
+  protected players = signal(AVAILABLE_CHARACTERS);
 
-    protected cycle_player_state(player_to_update: typeof AVAILABLE_CHARACTERS[number]): void {
-      const current_state_index = states.indexOf(player_to_update.state);
+  protected cycle_player_state(player_to_update: typeof AVAILABLE_CHARACTERS[number]): void {
+    const current_state_index = states.indexOf(player_to_update.state);
 
-      this.players.update((players) => players.map(player => {
-        if(player.name !== player_to_update.name) {
-          return player;
-        }
-        player.state = states[(current_state_index + 1) % states.length];
+    this.players.update((players) => players.map(player => {
+      if (player.image !== player_to_update.image) {
         return player;
-      }));
+      }
+      player.state = states[(current_state_index + 1) % states.length];
+      return player;
+    }));
+  }
 
-    }
+  protected get_human_player_index(index: number): number {
+    return this.players()
+      .slice(0, index)
+      .filter(p => p.state === 'human')
+      .length;
+  }
+
+  protected async start(): Promise<void> {
+    console.log('players', this.players().filter(p => p.state !== 'inactive'));
+  }
 }
