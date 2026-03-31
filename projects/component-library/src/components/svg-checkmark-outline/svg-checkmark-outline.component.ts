d717d1ece1503acd67406c0e87f3e6345b97f077
Craig Wayne <craigiswayne@gmail.com>
2026-03-31 15:07:22 +0100

    Lint fixes

--- projects/component-library/src/components/svg-checkmark-outline/svg-checkmark-outline.component.ts
+++ projects/component-library/src/components/svg-checkmark-outline/svg-checkmark-outline.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'mbr-svg-checkmark-outline',
   templateUrl: './svg-checkmark-outline.component.html',
   styleUrl: './svg-checkmark-outline.component.scss',
