76030b544b2bb2ce0254879acdfd2f517c1918cb
Craig Wayne <craigiswayne@gmail.com>
2026-04-01 15:28:48 +0100

    Extra items for the marquee

--- projects/component-library/src/components/marquee/marquee.stories.ts
+++ projects/component-library/src/components/marquee/marquee.stories.ts
@@ -58,6 +58,11 @@ export const Sample: Story = {
               <div>3</div>
               <div>4</div>
               <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
             </ng-template>
         </mbr-marquee>
       `
