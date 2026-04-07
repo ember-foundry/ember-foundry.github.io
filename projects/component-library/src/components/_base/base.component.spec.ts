045332a1892dc882f06a06f5ef3aa776dba2cff2
Craig Wayne <craigiswayne@gmail.com>
2026-04-07 14:29:21 +0100

    More tests for the base component

--- projects/component-library/src/components/_base/base.component.spec.ts
+++ projects/component-library/src/components/_base/base.component.spec.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, signal} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {describe, it, expect, beforeEach, vi} from 'vitest';
 import {BaseComponent} from './base.component';
@@ -6,17 +6,23 @@ import {BaseComponent} from './base.component';
 type ExposedBaseComponent = BaseComponent & { component_to_css_class: () => string, computed_host_css_classes_from: () => string };
 
 @Component({
-  template: ''
+  template: 'TestBaseHostComponent',
+  host: {
+    '[class]': 'host_css_classes()'
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 class TestBaseHostComponent extends BaseComponent {
-  readonly size = signal('sm');
-  readonly status = signal('success');
+  readonly size = input('sm');
+  readonly status = input('success');
+  protected readonly host_css_classes = this.computed_host_css_classes_from('size', 'status');
 }
 
 describe('BaseComponent', () => {
   let fixture: ComponentFixture<TestBaseHostComponent>;
   let component: TestBaseHostComponent;
   let spy_component_to_css_class: ReturnType<typeof vi.spyOn>;
+  let spy_computed_host_css_classes_from: ReturnType<typeof vi.spyOn>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,6 +33,8 @@ describe('BaseComponent', () => {
     component = fixture.componentInstance;
     let _component = component as unknown as ExposedBaseComponent
     spy_component_to_css_class = vi.spyOn(_component, 'component_to_css_class');
+    spy_computed_host_css_classes_from = vi.spyOn(_component, 'computed_host_css_classes_from');
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
@@ -46,4 +54,20 @@ describe('BaseComponent', () => {
   it('should apply the kebab-case class to the host element', () => {
     expect(fixture.nativeElement).toHaveClass('test-base-host');
   });
+
+  it('Should auto create css classes from component inputs', () => {
+    expect(spy_computed_host_css_classes_from).toHaveBeenCalled();
+  });
+
+  it('should have the correct host css classes based on the inputs', () => {
+    expect(fixture.nativeElement).toHaveClass('size-sm', 'status-success');
+  });
+
+  it('should update host css classes when inputs change', () => {
+    fixture.componentRef.setInput('size', 'lg');
+    fixture.componentRef.setInput('status', 'busy');
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toHaveClass('size-lg', 'status-busy');
+  });
+
 });
