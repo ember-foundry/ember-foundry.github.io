696a740a609b3e436f87f182d8850dae38ac2acc
Craig Wayne <craigiswayne@gmail.com>
2026-04-21 10:57:11 +0100

    SVG Checkmark Outline now tests visually

--- projects/component-library/src/components/svg-checkmark-outline/svg-checkmark-outline.component.spec.ts
+++ projects/component-library/src/components/svg-checkmark-outline/svg-checkmark-outline.component.spec.ts
@@ -5,6 +5,7 @@ import {SVGCheckmarkOutlineComponent} from './svg-checkmark-outline.component';
 describe('CheckmarkOutlineComponent', () => {
   let component: SVGCheckmarkOutlineComponent;
   let fixture: ComponentFixture<SVGCheckmarkOutlineComponent>;
+  let host: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,16 @@ describe('CheckmarkOutlineComponent', () => {
 
     fixture = TestBed.createComponent(SVGCheckmarkOutlineComponent);
     component = fixture.componentInstance;
+    host = fixture.nativeElement;
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should match visually', async() => {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    await expect(host).toMatchScreenshot('vuexy-theme-customizer-trigger');
   });
 });
