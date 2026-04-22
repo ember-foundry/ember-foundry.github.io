a29f0c6e22c767c7edbc0a82f5ebf82bd726c6b5
Craig Wayne <craigiswayne@gmail.com>
2026-04-22 13:55:49 +0100

    Updated storybook and reworked the github action progress svg

--- projects/component-library/src/components/github/action-progress/action-progress.stories.ts
+++ projects/component-library/src/components/github/action-progress/action-progress.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import type {Meta, StoryObj} from '@storybook/angular';
 import {ActionProgressComponent} from 'component-library';
 
 const meta: Meta<ActionProgressComponent> = {
