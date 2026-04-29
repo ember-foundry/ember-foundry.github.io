238bcfe7fe29655e077766666e3bd1f98d899712
Craig Wayne <craigiswayne@gmail.com>
2026-04-29 12:18:01 +0100

    Working Checkpoint 2

--- projects/component-library/src/components/lando-norris/lando-norris-sliding-text/lando-norris-sliding-text.component.ts
+++ projects/component-library/src/components/lando-norris/lando-norris-sliding-text/lando-norris-sliding-text.component.ts
@@ -1,13 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 
 @Component({
   //eslint-disable-next-line @angular-eslint/component-selector
   selector: 'mbr-lando-norris-sliding-text',
   imports: [],
   templateUrl: './lando-norris-sliding-text.component.html',
-  styleUrl: './lando-norris-sliding-text.component.scss',
+  styleUrls: [
+    '../lando-norris-fonts.scss',
+    './lando-norris-sliding-text.component.scss'
+  ],
+  host: {
+    'class': 'mona-sans'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandoNorrisSlidingTextComponent {
-
+  public text = input.required<string[], string>({transform: (received_value:string) => received_value.split('').map(_ => _.toUpperCase())});
 }
--- projects/component-library/src/components/lando-norris/lando-norris-sliding-text/lando-norris-sliding-text.stories.ts
+++ projects/component-library/src/components/lando-norris/lando-norris-sliding-text/lando-norris-sliding-text.stories.ts
@@ -11,7 +11,12 @@ const meta: Meta<LandoNorrisSlidingTextComponent> = {
       }
     }
   },
-  tags: ['autodocs', 'text', 'animation']
+  tags: ['autodocs', 'text', 'animation'],
+  argTypes: {
+    text: {
+      control: {type: 'text'}
+    }
+  }
 }
 
 export default meta;
@@ -20,6 +25,36 @@ type Story = Meta<LandoNorrisSlidingTextComponent>;
 
 export const Sample: Story = {
   parameters: {
-    layout: 'centered'
+    layout: 'fullscreen'
+  },
+  args: {
+    text: 'PERMISSIONS'
+  },
+  render: (args) => {
+    return {
+      props: args,
+      styles: [
+        `:host{
+          align-items: center;
+          background-color: #d2ff00;
+          background-color: #282c20;
+          color: black;
+          color: #dde1d2;
+          color: white;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          min-height: 3rem;
+          padding: 2rem 0;
+          width: 100%;
+        }`
+      ],
+      template: `
+        <mbr-lando-norris-sliding-text text="tiktok" />
+        <mbr-lando-norris-sliding-text text="instagram" />
+        <mbr-lando-norris-sliding-text text="youtube" />
+        <mbr-lando-norris-sliding-text text="twitch" />
+      `
+    }
   }
 }
