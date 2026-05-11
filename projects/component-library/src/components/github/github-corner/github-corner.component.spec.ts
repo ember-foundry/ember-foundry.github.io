8606a8a314d12e5c6b7aa44ec3da956ff966c26a
Craig Wayne <craigiswayne@gmail.com>
2026-05-11 12:56:34 +0100

    Github Corner

--- projects/component-library/src/components/github/github-corner/github-corner.component.spec.ts
+++ projects/component-library/src/components/github/github-corner/github-corner.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { describe, it, expect } from 'vitest';
 import { GithubCornerComponent } from './github-corner.component';
 
 describe('GithubCornerComponent', () => {
@@ -10,7 +10,7 @@ describe('GithubCornerComponent', () => {
     await TestBed.configureTestingModule({
       imports: [GithubCornerComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(GithubCornerComponent);
     component = fixture.componentInstance;
