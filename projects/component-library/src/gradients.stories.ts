d84cf3bdf6762d6b1d382d5b32a0f97b1996674f
Craig Wayne <craigiswayne@gmail.com>
2026-05-14 12:39:01 +0100

    Gradient Showcase done

--- projects/component-library/src/gradients.stories.ts
+++ projects/component-library/src/gradients.stories.ts
@@ -14,6 +14,10 @@ const gradients = [
       name: 'Trello',
       url: 'https://trello.com/'
     }
+  },
+  {
+    label: 'Matchday Experience',
+    value: 'linear-gradient(to right bottom, oklab(0.766529 -0.00252706 0.138662 / 0.2) 0%, oklab(0.766529 -0.00252706 0.138662 / 0.05) 100%)'
   }
 ];
 
@@ -39,6 +43,8 @@ export const Gradients: StoryObj = {
             flex-direction: column;
           }
 
+          .card h4 {margin: 0;}
+
           .card .content {
             display: inline-flex;
             flex-grow: 1;
@@ -59,28 +65,65 @@ export const Gradients: StoryObj = {
           .card a {
             color: inherit;
           }
+
+
+        .card header,
+        .card footer {
+          display: inline-flex;
+          justify-content: space-between;
+        }
+
+        .card button {
+          appearance: none;
+          outline: none;
+          border: none;
+          background: none;
+          position: relative;
+          cursor: pointer;
+        }
+
+        .card button:before {
+          content: '';
+          width: 10px;
+          background: black;
+          height: 1px;
+          position: absolute;
+          top: 100%;
+          right: unset;
+          left: 10px;
+          transition: all 0.2s ease-in;
+        }
+
+        .card button:hover:before {
+          width: calc(100% - 20px);
+        }
         `
       ],
       template:
         `
           <h1>Gradients</h1>
+          <p>Inspired by <a href="https://webgradients.com/" target="_blank">webgradients.com</a></p>
           <div class="grid">
           ${
           gradients
-            .map(item => `
-                <div class="card">
-                    <header>
-                       ${item.label}
-                    </header>
-                    <div class="content">
-                        <div class="preview" style="background-image: ${item.value}"></div>
-                    </div>
-                    <footer>
-                        As seen on <a href='${item.citation.url}' target='_blank'>${item.citation.name}</a>
-                    </footer>
-                </div>
-            `)
-            .join(' ')
+            .map(item => {
+              const footer_left_text = item.citation ? `As seen on <a href='${item.citation.url}' target='_blank'>${item.citation.name}</a>` : '';
+              return `
+                  <div class="card">
+                      <header>
+                         <h4>${item.label}</h4>
+                      </header>
+                      <div class="content">
+                          <div class="preview" style="background-image: ${item.value}"></div>
+                      </div>
+                      <footer>
+                          <span>${footer_left_text}</span>
+                          <button>Copy CSS</button>
+                      </footer>
+                  </div>
+              `;
+            })
+            .join('')
           }
           </div>
         `
