01fcb34ea85aecddb5fc14ed56ca91b28ffa9e0e
Craig Wayne <craigiswayne@gmail.com>
2026-05-26 18:08:57 +0100

    Small style change for the story

--- projects/component-library/src/calculators/flex-flow/flex-flow.stories.ts
+++ projects/component-library/src/calculators/flex-flow/flex-flow.stories.ts
@@ -12,10 +12,10 @@ const meta: Meta<FlexFlowComponent> = {
     styles: [
       `
         :host {
-          --grid-color-x: #f0f0f0;
+         --grid-color-x: #0000001a;
          --grid-color-y: var(--grid-color-x);
          --grid-line-thickness: 1px;
-         --grid-size: 30px;
+         --grid-size: calc(100vw / 12);
          background-repeat: repeat;
          background-image: radial-gradient(white, transparent), linear-gradient(90deg, transparent 0, transparent calc(var(--grid-size) - var(--grid-line-thickness)), var(--grid-color-y) calc(var(--grid-size) - var(--grid-line-thickness)), var(--grid-color-y) var(--grid-size)), linear-gradient(180deg, transparent 0, transparent calc(var(--grid-size) - var(--grid-line-thickness)), var(--grid-color-x) calc(var(--grid-size) - var(--grid-line-thickness)), var(--grid-color-x) var(--grid-size));
          background-size: 100%, var(--grid-size) 100%, 100% var(--grid-size);
