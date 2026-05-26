425715f9efbaa239ae6a3ebf926cf792178996f5
Craig Wayne <craigiswayne@gmail.com>
2026-05-26 17:56:54 +0100

    Flex Flow Shorthand calculator

--- projects/component-library/src/calculators/flex-flow/flex-flow.component.ts
+++ projects/component-library/src/calculators/flex-flow/flex-flow.component.ts
@@ -1,11 +1,17 @@
 import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgTemplateOutlet} from '@angular/common';
+import {TooltipDirective} from 'component-library';
+import {CdkCopyToClipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'mbr-flex-flow',
-  imports: [FormsModule],
+  imports: [FormsModule, NgTemplateOutlet, TooltipDirective, CdkCopyToClipboard],
   templateUrl: './flex-flow.component.html',
   styleUrl: './flex-flow.component.scss',
+  host: {
+    '[class]': 'result()'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FlexFlowComponent {
@@ -18,4 +24,5 @@ export class FlexFlowComponent {
 
     return [dir, wrap].filter(i => !!i).join(' ') || 'none';
   });
+
 }
