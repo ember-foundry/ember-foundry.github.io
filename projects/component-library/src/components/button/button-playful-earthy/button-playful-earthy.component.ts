69e18b384ce338de6351516dc9a71bf8a6f1b9b6
Craig Wayne <craigiswayne@gmail.com>
2026-05-29 14:37:27 +0100

    Primary, secondary and disabled buttons for the playful earthy varb

--- projects/component-library/src/components/button/button-playful-earthy/button-playful-earthy.component.ts
+++ projects/component-library/src/components/button/button-playful-earthy/button-playful-earthy.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {BaseComponent} from '../../_base/base.component';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -8,8 +9,13 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     '../button.component.scss',
     './button-playful-earthy.component.scss'
   ],
+  host: {
+    '[class]': 'host_css_classes()'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonPlayfulEarthyComponent {
-
+export class ButtonPlayfulEarthyComponent extends BaseComponent {
+  public size = input<'small'|undefined>();
+  public color  = input<'primary'|'secondary'|undefined>();
+  protected readonly host_css_classes = this.computed_host_css_classes_from('size', 'color');
 }
--- projects/component-library/src/components/button/button.stories.ts
+++ projects/component-library/src/components/button/button.stories.ts
@@ -1,5 +1,6 @@
 import {Meta, moduleMetadata, StoryObj} from '@storybook/angular';
 import {ButtonBasicComponent, ButtonMonochromaticComponent, ButtonPlayfulEarthyComponent} from 'component-library';
+import {NgTemplateOutlet} from '@angular/common';
 
 const meta: Meta = {
   decorators: [
@@ -7,7 +8,8 @@ const meta: Meta = {
       imports: [
         ButtonBasicComponent,
         ButtonMonochromaticComponent,
-        ButtonPlayfulEarthyComponent
+        ButtonPlayfulEarthyComponent,
+        NgTemplateOutlet
       ]
     })
   ],
@@ -125,167 +127,161 @@ export const Monochromatic: StoryObj = {
 }
 
 export const PlayfulEarthy: StoryObj = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'A playful, earthy-themed button with a subtle shadow effect and a muted color palette.'
+      }
+    }
+  },
   render: (args) => ({
     props: args,
     styles: [
-      `:host {
+      `section {
         align-items: center;
         display: grid;
         grid-template-columns: 1fr auto auto auto auto;
         column-gap: 8px;
         justify-items: flex-start;
+        border-bottom: 5px solid #f0f0f0;
+        padding-bottom: 1rem;
+        margin-bottom: 1rem;
       }`
     ],
     template: `
-      <p>Description</p>
-      <p>Standard</p>
-      <p>Primary</p>
-      <p>Accent</p>
-      <p>Disabled</p>
+      <section>
+        <p class="font-bold">Size: Standard</p>
+        <p class="font-bold">Standard</p>
+        <p class="font-bold">Primary</p>
+        <p class="font-bold">Secondary</p>
+        <p class="font-bold">Disabled</p>
 
-      <p>No content using "button" tag</p>
-      <button mbrButtonPlayfulEarthy></button>
-      <button mbrButtonPlayfulEarthy></button>
-      <button mbrButtonPlayfulEarthy></button>
-      <button mbrButtonPlayfulEarthy disabled></button>
+        <p>No content using "button" tag</p>
+        <button mbrButtonPlayfulEarthy></button>
+        <button mbrButtonPlayfulEarthy color="primary"></button>
+        <button mbrButtonPlayfulEarthy color="secondary"></button>
+        <button mbrButtonPlayfulEarthy disabled></button>
 
-      <p>No content using  "a" tag</p>
-      <a mbrButtonPlayfulEarthy></a>
-      <a mbrButtonPlayfulEarthy></a>
-      <a mbrButtonPlayfulEarthy></a>
-      <a mbrButtonPlayfulEarthy disabled></a>
+        <p>No content using  "a" tag</p>
+        <a mbrButtonPlayfulEarthy></a>
+        <a mbrButtonPlayfulEarthy color="primary"></a>
+        <a mbrButtonPlayfulEarthy color="secondary"></a>
+        <a mbrButtonPlayfulEarthy disabled></a>
 
-      <p>Projected Content using "button" tag</p>
-      <button mbrButtonPlayfulEarthy>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-            <polyline points="7 10 12 15 17 10"/>
-            <line x1="12" x2="12" y1="15" y2="3"/>
-          </svg>
-          View
+        <p>Projected Content using "button" tag</p>
+        <button mbrButtonPlayfulEarthy>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+          Add
         </button>
-      <button mbrButtonPlayfulEarthy>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-            <polyline points="7 10 12 15 17 10"/>
-            <line x1="12" x2="12" y1="15" y2="3"/>
-          </svg>
-          View
-        </button>
-      <button mbrButtonPlayfulEarthy>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-            <polyline points="7 10 12 15 17 10"/>
-            <line x1="12" x2="12" y1="15" y2="3"/>
-          </svg>
-          View
-        </button>
-      <button mbrButtonPlayfulEarthy disabled>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-          <polyline points="7 10 12 15 17 10"/>
-          <line x1="12" x2="12" y1="15" y2="3"/>
-        </svg>
-        View
-      </button>
+        <button mbrButtonPlayfulEarthy color="primary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
 
-      <p>Projected Content using "a" tag </p>
-      <a mbrButtonPlayfulEarthy>
+            Add
+          </button>
+        <button mbrButtonPlayfulEarthy color="secondary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+            Add
+          </button>
+        <button mbrButtonPlayfulEarthy disabled>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+            Add
+          </button>
+
+        <p>Projected Content using "a" tag </p>
+        <a mbrButtonPlayfulEarthy>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+          Add
+        </a>
+        <a mbrButtonPlayfulEarthy color="primary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+          Add
+        </a>
+        <a mbrButtonPlayfulEarthy color="secondary">
+            <ng-container [ngTemplateOutlet]="plus_icon" />
+
+            Add
+        </a>
+        <a mbrButtonPlayfulEarthy disabled>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+
+          Add
+        </a>
+
+        <p class="font-bold">Size: Small</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <p>No content using "button" tag</p>
+        <button mbrButtonPlayfulEarthy size="small"></button>
+        <button mbrButtonPlayfulEarthy size="small" color="primary"></button>
+        <button mbrButtonPlayfulEarthy size="small" color="secondary"></button>
+        <button mbrButtonPlayfulEarthy size="small" disabled ></button>
+
+        <p>No content using  "a" tag</p>
+        <a mbrButtonPlayfulEarthy size="small"></a>
+        <a mbrButtonPlayfulEarthy size="small" color="primary"></a>
+        <a mbrButtonPlayfulEarthy size="small" color="secondary"></a>
+        <a mbrButtonPlayfulEarthy size="small" disabled></a>
+
+        <p>Projected Content using "button" tag</p>
+        <button mbrButtonPlayfulEarthy size="small">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </button>
+        <button mbrButtonPlayfulEarthy size="small" color="primary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </button>
+        <button mbrButtonPlayfulEarthy size="small" color="secondary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </button>
+        <button mbrButtonPlayfulEarthy size="small" disabled>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </button>
+
+        <p>Projected Content using "a" tag </p>
+        <a mbrButtonPlayfulEarthy size="small">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </a>
+        <a mbrButtonPlayfulEarthy size="small" color="primary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </a>
+        <a mbrButtonPlayfulEarthy size="small" color="secondary">
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </a>
+        <a mbrButtonPlayfulEarthy size="small" disabled>
+          <ng-container [ngTemplateOutlet]="plus_icon" />
+          Add
+        </a>
+      </section>
+      <ng-template #plus_icon>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
           fill="none"
+          xmlns="http://www.w3.org/2000/svg"
           stroke="currentColor"
-          stroke-width="2"
+          stroke-width="1.75"
           stroke-linecap="round"
           stroke-linejoin="round"
         >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-          <polyline points="7 10 12 15 17 10"/>
-          <line x1="12" x2="12" y1="15" y2="3"/>
+          <path d="M2.91666 7H11.0833" />
+          <path d="M7 2.91663V11.0833" />
         </svg>
-        View
-      </a>
-      <a mbrButtonPlayfulEarthy>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-          <polyline points="7 10 12 15 17 10"/>
-          <line x1="12" x2="12" y1="15" y2="3"/>
-        </svg>
-        View
-      </a>
-      <a mbrButtonPlayfulEarthy>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-          <polyline points="7 10 12 15 17 10"/>
-          <line x1="12" x2="12" y1="15" y2="3"/>
-        </svg>
-        View
-      </a>
-      <a mbrButtonPlayfulEarthy disabled>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-          <polyline points="7 10 12 15 17 10"/>
-          <line x1="12" x2="12" y1="15" y2="3"/>
-        </svg>
-        View
-      </a>
+      </ng-template>
     `
   })
 }
