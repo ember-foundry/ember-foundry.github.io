0dc6941eb8d41b9f0a9d2f1a60178a6c45a1e10e
Craig Wayne <craigiswayne@gmail.com>
2026-06-11 15:40:40 +0100

    Lucide Package added for icons

--- projects/critter/src/components/footer/footer.component.ts
+++ projects/critter/src/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ButtonComponent} from '../button/button.component';
 import {BadgeDirective} from '../badge/badge.directive';
+import {LucideBox, LucideHouse, LucideList, LucideShoppingCart, LucideUsers} from '@lucide/angular';
 
 @Component({
   selector: 'mbr-critter-footer',
   imports: [
     ButtonComponent,
-    BadgeDirective
+    BadgeDirective,
+    LucideHouse,
+    LucideList,
+    LucideBox,
+    LucideUsers,
+    LucideShoppingCart
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
